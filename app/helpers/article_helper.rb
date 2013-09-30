1dea277b1130be79645d3f212d01c17041d7a8ae
dklounge <daviddarden11@gmail.com>
2013-09-30 15:55:59 -0700

    fixed sentence rendering, used Americas cup page

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,6 +1,6 @@
 module ArticleHelper
  def random
-  response = RestClient.get('http://en.wikipedia.org/wiki/Special:Random')
+  response = RestClient.get('http://en.wikipedia.org/wiki/2013_America%27s_Cup')
   page = Nokogiri::HTML(response)
  end
 end
